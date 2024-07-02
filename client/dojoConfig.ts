2fff3456543fd66ee9f4cac2b96571f85c6f486d
Peter Kagwe <kagwepeter07@gmail.com>
2024-07-02 19:34:48 +0300

    deploy to slot

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -1,12 +1,13 @@
 import manifest from "./manifest.json";
 import { createDojoConfig } from "@dojoengine/core";
 
-// export const dojoConfig = createDojoConfig({
-//     manifest,
-//     rpcUrl: "/api" // Use proxy endpoint for rpcUrl
-//   });
-
-
-  export const dojoConfig = createDojoConfig({
-    manifest
+export const dojoConfig = createDojoConfig({
+    manifest,
+    rpcUrl: "https://api.cartridge.gg/x/color-grid-demo/katana",
+    toriiUrl: "https://api.cartridge.gg/x/color-grid-demo/torii"
   });
+
+
+  // export const dojoConfig = createDojoConfig({
+  //   manifest
+  // });
