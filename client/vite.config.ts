671dfd11077d03d0681c185d5274f1453ad09fbc
Peter Kagwe <kagwepeter07@gmail.com>
2024-07-02 19:10:01 +0300

    deploy to slot

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     server:{
     proxy: {
         '/api': {
-            target: 'http://localhost:5050',
+            target: 'https://color-grid-dojo.vercel.app',
             changeOrigin: true,
             rewrite: (path) => path.replace(/^\/api/, '')
         }
